eae237e1ee34100ff5faa0aa043af43f5a1c75ba
Gabriel Susai <gabriel.susai@gmail.com>
2014-04-05 15:03:26 -0400

    Added code to display images from file system and bing search

---
+++ Invitar/Scripts/custom.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $(document).on("click", ".img-photo", function () {
+        $(".img-photo").removeClass("selected-image");
+        $(this).addClass("selected-image");
+    });
+    $('#btnSearch').click(function () {
+        var query = $('#txtSearch').val();
+        
+        if (query == null) {
+            aler('Enter search query');
+            return false;
+        }
+        $.ajax({
+            type: "POST",
+            url: "/Event/WebSearch",
+            data: { 'query': query},
+            datatype: "html",
+            success: function (data) {
+                $('#result').html(data);
+            }
+        });
+    });
+});
+
+
