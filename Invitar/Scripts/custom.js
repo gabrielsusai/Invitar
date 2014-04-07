8782001ca8facd25f3532280fcd909f3ccc7a743
mpillai <mkp.pillai@gmail.com>
2014-04-07 00:02:21 -0400

    Implemented Google API for location auto complete.

--- Invitar/Scripts/custom.js
+++ Invitar/Scripts/custom.js
@@ -20,6 +20,37 @@
             }
         });
     });
+
+    
 });
+
+var autocomplete;
+
+function initializeGooglePlace() {
+    // Create the autocomplete object, restricting the search
+    // to geographical location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        (document.getElementById('Location')),
+        { types: ['geocode'] });
+    // When the user selects an address from the dropdown,
+    // populate the address fields in the form.
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        fillInAddress();
+    });
+}
+
+// [START region_geolocation]
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var geolocation = new google.maps.LatLng(
+                position.coords.latitude, position.coords.longitude);
+            autocomplete.setBounds(new google.maps.LatLngBounds(geolocation,
+                geolocation));
+        });
+    }
+}
 
 
